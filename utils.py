7b72181972fd248c8dcd4da8704dc89b950bb003
Shital Shah <sytelus@gmail.com>
2019-12-20 13:15:16 -0800

    defaults set for direct run

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import  os
+from os.path import exists
 import  numpy as np
 import  torch
 import  shutil
@@ -128,13 +129,16 @@ def drop_path(x, drop_prob):
 
 
 def create_exp_dir(path, scripts_to_save=None):
+    path = os.path.expanduser(path)
     if not os.path.exists(path):
-        os.mkdir(path)
+        os.makedirs(path, exist_ok=True)
     print('Experiment dir : {}'.format(path))
 
     if scripts_to_save is not None:
         if not os.path.exists(os.path.join(path, 'scripts')):
-            os.mkdir(os.path.join(path, 'scripts'))
+            os.makedirs(os.path.join(path, 'scripts'), exist_ok=True)
         for script in scripts_to_save:
             dst_file = os.path.join(path, 'scripts', os.path.basename(script))
             shutil.copyfile(script, dst_file)
+
+    return path
